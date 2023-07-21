5096f3ee84dcb89be0842d336bfab869329a1f18
chamox <sezuniga1@uc.cl>
2023-07-20 23:49:56 -0400

    Add comments to check_permutation

--- chapter_1/check_permutation.py
+++ chapter_1/check_permutation.py
@@ -1,6 +1,11 @@
 # Given two strings, write a method to decide if one is a permutation of the
 # other.
 
+# hints: Try a hash table
+#       Could a bit vector be useful?   (bit vector is a vector of bits)
+#       Can you solve it in O(N log N) time? What might a solution like that look like?
+
+
 def check_permutation(string_1: str, string_2: str) -> bool:
     """
     sorted function is N*log(N)    
@@ -27,7 +32,7 @@ def check_permutation_2(string_1: str, string_2: str) -> bool:
 
     for character in string_1: # O(N)
 
-        if character in char_counter.keys():
+        if character in char_counter.keys(): # O(1) in avg case, O(N) worst case (https://wiki.python.org/moin/TimeComplexity)
             char_counter[character] += 1
         else:
             char_counter[character] = 1
@@ -43,8 +48,6 @@ def check_permutation_2(string_1: str, string_2: str) -> bool:
         return True
     else:
         return False
-
-
 
 if __name__ == "__main__":
 
@@ -68,7 +71,6 @@ if __name__ == "__main__":
                     ]
 
     for function in functions:
-
     
         print("\ntesting",function.__name__+":")
 
