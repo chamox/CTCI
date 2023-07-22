c87767902b4884f775faff63faae5b9c0ce4ecb4
chamox <sezuniga1@uc.cl>
2023-07-22 01:34:09 -0400

    Fix exercises names

--- chapter_1/1_1-is_unique.py
+++ chapter_1/1_1-is_unique.py
@@ -1,6 +1,10 @@
 # Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
 # cannot use additional data structures?
 
+# hints: Try a hash table
+#       Could a bit vector be useful?   (bit vector is a vector of bits)
+#       Can you solve it in O(N log N) time? What might a solution like that look like?
+
 # test cases:
 
 # "abcdef" -> True
@@ -11,11 +15,11 @@ import string as string_lib
 # Implementation with dictionaries
 def is_unique(string: str) -> bool:
     
-    string = string.replace(" ","").lower()
+    string = string.replace(" ","").lower() # we prevent edge cases with this
 
     dictionary = dict()
 
-    for char in string:
+    for char in string: # O(N) 
         if char in dictionary.keys():
             return False
         else:
@@ -23,14 +27,14 @@ def is_unique(string: str) -> bool:
         
     return True
 
-# Implementation with no adsitional data structure
+# Implementation with no additional data structure
 def is_unique_v2(string: str) -> bool:
 
     string = string.replace(" ","").lower()
 
     alphabet = string_lib.printable
 
-    for character in string:
+    for character in string: # O(N) 
         if character in alphabet:
             alphabet = alphabet.replace(character,"") # we need to reasign this variable.
         else:
@@ -52,11 +56,11 @@ if __name__ == "__main__":
                  ]
 
     for function in functions:
-
-      
-        print("testing",function.__name__,":")
+            
+        print("testing",function.__name__+":")
 
         for text,result in test_cases:
-            print(function(text) == result)
- 
-        print("\n")
+            assert function(text) == result
+            # print(function(text) == result)
+
+        print("All test passed\n")
--- chapter_1/1_3-URLify.py
+++ chapter_1/1_3-URLify.py
@@ -2,6 +2,7 @@
 # has sufficient space at the end to hold the additional characters, and that you are given the "true"
 # length of the string. (Note: If implementing in Java, please use a character array so that you can
 # perform this operation in place.)
+
 # EXAMPLE
 # Input: "Mr John Smith ", 13
 # Output: "Mr%20John%20Smith"
